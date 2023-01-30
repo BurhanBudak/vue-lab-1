185ac5f0bc984c0e89e42e2a18df75ec0c498c85
BurhanBudak <budak.burha@gmail.com>
2023-01-30 23:27:37 +0100

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,7 @@
 <footer class="page-footer font-small bg-primary">
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3 white">© 2020 Copyright:
-    <a href="/"> MDBootstrap.com</a>
+  <div class="footer-copyright text-center py-3 white">© 2020 Copyright: Burhan Budak
   </div>
   <!-- Copyright -->
 
